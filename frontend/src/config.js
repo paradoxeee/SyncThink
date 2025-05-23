62b4c165aec21a91cf256c058db2c31c603d9cc3
paradoxee <gualtieri.giiiaannii@gmail.com>
2025-05-23 23:22:08 +0200

    fichier config creer et game.js edit

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,7 +2,7 @@ const config = {
   // En développement, utilise l'URL locale
   // En production, utilise l'URL du backend déployé
   backendUrl: import.meta.env.PROD 
-    ? 'https://syncthink.onrender.com'  // URL de votre backend Render
+    ? 'https://web-production-5d0b9.up.railway.app/'  // URL de votre backend Railway
     : 'http://localhost:3000'
 };
 
